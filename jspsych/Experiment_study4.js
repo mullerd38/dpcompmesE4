3b5605edd9ea13f71280b9187084a075389c2735
mullerd38 <dominique.muller@univ-grenoble-alpes.fr>
2026-03-23 12:55:38 +0100

    parentheses3

--- jspsych/Experiment_study4.js
+++ jspsych/Experiment_study4.js
@@ -37,7 +37,7 @@ var welcome = {
   type: jsPsychHtmlButtonResponse,
   stimulus:
     "<h1 class ='custom-title'>Welcome</h1>" +
-    "<p class='instructions'>TEST 9 Thank you for taking part in this survey. <b> Please note that you can only participate from a computer.</b> </p>" +
+    "<p class='instructions'>TEST 10 Thank you for taking part in this survey. <b> Please note that you can only participate from a computer.</b> </p>" +
     "<p class='instructions'>We are going to ask you to imagine you are a medical researcher who wants to test the effectiveness of a medicine against a fictitious disease. " +
     "Your task will be to give your opinion on the effectiveness of this medicine.</p>" +
     "<p class='instructions'>If you have any question related to this research, please " +
@@ -256,7 +256,7 @@ var slider = {
     // Only return the question text here
     return `
         <p style="margin-bottom: 1px;">${questionText}</p>
-        <p style="font-size: 16px;">Your current answer: <strong><span id="answer-display">1</span></strong></p>`;
+        <p style="font-size: 16px;">(Your current answer: <strong><span id="answer-display">1</span></strong>)</p>`;
   },
   slider_width: 700 , // Keep this as is to control slider size
 
